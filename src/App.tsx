f2546c8658348f51499d419401d2c38575282148
arimariojesus <arimario.jesus@hotmail.com>
2023-08-12 00:00:48 -0300

    feat: integrates navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Container } from '@chakra-ui/react';
 import { Header } from '@/components/Header';
+import { Navbar } from '@/components/Navbar';
 
 function App() {
   return (
     <Box w="100%">
       <Header />
+
+      <Container maxW="container.lg">
+        <Navbar />
+      </Container>
     </Box>
   );
 }
