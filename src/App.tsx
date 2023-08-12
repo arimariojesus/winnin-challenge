cd00c0da4fc5c10db283a2ad914307d3404c8143
arimariojesus <arimario.jesus@hotmail.com>
2023-08-12 03:18:42 -0300

    feat: integrates articles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import { Box, Container } from '@chakra-ui/react';
+import { Box, Container, Divider, VStack } from '@chakra-ui/react';
+
 import { Header } from '@/components/Header';
 import { Navbar } from '@/components/Navbar';
+import { Article } from '@/components/Article';
+import { hotJsonData } from '@/mocks';
+import { ArticleMapper } from '@/types/Article';
+import { ChildrenResponse } from '@/types/Reddit';
+
+const articles = hotJsonData.data.children.map((children) => new ArticleMapper(children as ChildrenResponse));
 
 function App() {
   return (
@@ -9,6 +16,12 @@ function App() {
 
       <Container maxW="container.lg">
         <Navbar />
+
+        <VStack spacing={3} my={8} divider={<Divider />}>
+          {articles.map((article) => (
+            <Article key={article.id} article={article} />
+          ))}
+        </VStack>
       </Container>
     </Box>
   );
