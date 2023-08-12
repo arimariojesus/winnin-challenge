7eb58e50d2ccb197019d21ec905f3581e2b0a86b
arimariojesus <arimario.jesus@hotmail.com>
2023-08-11 23:42:00 -0300

    feat: add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,34 @@
+import { ReactIcon } from '@/assets/icons';
+import { Flex, HStack, Heading, Text } from '@chakra-ui/react';
+
+const CustomLogo = () => {
+  return (
+    <HStack>
+      <ReactIcon />
+      <Heading>
+        REACT
+        <Text as="span" color="brand.winnin">
+          JS
+        </Text>
+      </Heading>
+    </HStack>
+  );
+};
+
+export const Header = () => {
+  return (
+    <Flex
+      as="header"
+      p={6}
+      bg="brand.gray.900"
+      pos="sticky"
+      top="0"
+      justifyContent="center"
+      alignItems="center"
+      borderBottomWidth="1px"
+      borderBottomColor="brand.whiteAlpha.100"
+    >
+      <CustomLogo />
+    </Flex>
+  );
+};
