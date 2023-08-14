6dd8a6c9dfe3bb17659da8743b9ea7bc99eb3f99
arimariojesus <arimario.jesus@hotmail.com>
2023-08-13 21:47:37 -0300

    feat: composes app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { Box, Container, Divider, VStack } from '@chakra-ui/react';
+import { Box, Container } from '@chakra-ui/react';
 
-import { Header } from '@/components/Header';
-import { Navbar } from '@/components/Navbar';
-import { Article } from '@/components/Article';
-import { hotJsonData } from '@/mocks';
-import { ArticleMapper } from '@/types/Article';
-import { ChildrenResponse } from '@/types/Reddit';
-
-const articles = hotJsonData.data.children.map((children) => new ArticleMapper(children as ChildrenResponse));
+import Header from '@/components/Header';
+import Navbar from '@/components/Navbar';
+import ArticlesList from '@/components/ArticlesList';
 
 function App() {
   return (
@@ -17,11 +12,7 @@ function App() {
       <Container maxW="container.lg">
         <Navbar />
 
-        <VStack spacing={3} my={8} divider={<Divider />}>
-          {articles.map((article) => (
-            <Article key={article.id} article={article} />
-          ))}
-        </VStack>
+        <ArticlesList />
       </Container>
     </Box>
   );
