478424a74a46a02fcda5b4d099d95fa97b531b94
arimariojesus <arimario.jesus@hotmail.com>
2023-08-14 00:04:20 -0300

    feat(ArticleSkeleton): improve accesibility

--- src/components/Article/ArticleSkeleton.tsx
+++ src/components/Article/ArticleSkeleton.tsx
@@ -2,7 +2,16 @@ import { Skeleton, VStack } from '@chakra-ui/react';
 
 export const ArticleSkeleton = () => {
   return (
-    <VStack spacing={6} w="100%" p={6} borderRadius={16} alignItems="flex-start" bg="brand.whiteAlpha.50">
+    <VStack
+      spacing={6}
+      w="100%"
+      p={6}
+      borderRadius={16}
+      alignItems="flex-start"
+      bg="brand.whiteAlpha.50"
+      role="alert"
+      aria-busy="true"
+    >
       <Skeleton borderRadius="lg" w="140px" h="20px" />
       <Skeleton borderRadius="lg" w="100%" h="80px" />
       <Skeleton borderRadius="lg" w="70px" h="20px" />
